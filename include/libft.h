ead19097727e16d6e5cc0e644f327a64d49ae6e3
Tuomo Talvenheimo <tuomo.talvenheimo@outlook.com>
2023-03-29 11:02:58 +0300

    added free_array proto to header, forgot it last time

--- include/libft.h
+++ include/libft.h
@@ -53,4 +53,6 @@ void		ft_putendl_fd(char *s, int fd);
 int			ft_putnbr_fd(int n, int fd);
 int			ft_printf(const char *str, ...);
 int			ft_printf_fd(int fd, const char *str, ...);
+void		free_array(char **arr);
+
 #endif
